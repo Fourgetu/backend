d3c30edda74eea3804f97a9d740a0cd301906d26
Bin <q124375@gmail.com>
2026-09-13 09:23:01 +0800

    fix: wire runtime dependencies for API modules

--- src/modules/speed-limits/speed-limits.module.ts
+++ src/modules/speed-limits/speed-limits.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+
+import { AxiosModule } from '@common/axios';
 
 import { NodesModule } from '@modules/nodes/nodes.module';
 
+import { SpeedLimitsRepository } from './repositories/speed-limits.repository';
 import { SpeedLimitsController } from './speed-limits.controller';
 import { SpeedLimitsConverter } from './speed-limits.converter';
-import { SpeedLimitsRepository } from './repositories/speed-limits.repository';
 import { SpeedLimitsService } from './speed-limits.service';
 
 @Module({
-    imports: [NodesModule],
+    imports: [AxiosModule, CqrsModule, NodesModule],
     controllers: [SpeedLimitsController],
     providers: [SpeedLimitsConverter, SpeedLimitsRepository, SpeedLimitsService],
     exports: [SpeedLimitsService],
--- src/modules/user-routes/user-routes.module.ts
+++ src/modules/user-routes/user-routes.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
-import { SpeedLimitsModule } from '@modules/speed-limits/speed-limits.module';
+import { AxiosModule } from '@common/axios';
+
 import { NodesModule } from '@modules/nodes/nodes.module';
+import { SpeedLimitsModule } from '@modules/speed-limits/speed-limits.module';
 
+import { PortHoppingConfigsService } from './port-hopping-configs.service';
+import { PortRangeAllocator } from './port-range-allocator.service';
+import { UserRoutesRepository } from './repositories';
 import { UserRoutesController } from './user-routes.controller';
 import { UserRoutesConverter } from './user-routes.converter';
-import { UserRoutesRepository } from './repositories';
 import { UserRoutesService } from './user-routes.service';
-import { PortRangeAllocator } from './port-range-allocator.service';
-import { PortHoppingConfigsService } from './port-hopping-configs.service';
 
 @Module({
-    imports: [SpeedLimitsModule, NodesModule],
+    imports: [AxiosModule, CqrsModule, SpeedLimitsModule, NodesModule],
     controllers: [UserRoutesController],
     providers: [
         PortHoppingConfigsService,
